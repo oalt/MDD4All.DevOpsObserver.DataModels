5fe2522f1538e8760418751ec97d6e2a625bc9b9
oalt <oa@lacheweg21.de>
2024-07-25 17:37:58 +0200

    Add property for organization title

--- src/MDD4All.DevOpsObserver.DataModels/DevOpsConfiguration.cs
+++ src/MDD4All.DevOpsObserver.DataModels/DevOpsConfiguration.cs
@@ -4,6 +4,8 @@ namespace MDD4All.DevOpsObserver.DataModels
 {
     public class DevOpsConfiguration
     {
+        public string OrganizationTitle { get; set; } = string.Empty;
+
         public List<DevOpsSystem> Systems { get; set; } = new List<DevOpsSystem>();
     }
 }
